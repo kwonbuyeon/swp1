5e0cef84876ef159c569764e8c80cb2f009656ec
kwonbuyeon <qndusrnjs@gmail.com>
2020-06-26 22:26:02 +0900

    Issue #4 Change first result "sum: 0" -> "sum: here will show (a+b)" "product: 0" -> "product: here will show (a*b)"

--- calc/comp.py
+++ calc/comp.py
@@ -2,12 +2,15 @@ from cgi import parse_qs
 from compTemplate import html
 def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
-    a = d.get('a', '0')[0]
-    b = d.get('b', '0')[0]
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
     if '' not in [a, b]:
         a, b = int(a), int(b)
-	sum=a+b
-	product=a*b
+        sum=a+b
+        product=a*b
+    else:
+        sum ='here will show (a+b)'
+        product='here will show (a*b)'
     response_body= html +"sum: "+str(sum)+"<br>product: "+str(product)
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
