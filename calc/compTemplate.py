c935e70fa52e4aeb63302290f4ffc18275c581e3
kwonbuyeon <qndusrnjs@gmail.com>
2020-06-26 20:53:43 +0900

    issue #3

---
+++ calc/compTemplate.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+    <body>
+        <form>
+             a = <input type="number" name="a"><br>
+             b = <input type="number" name="b"><br><br>
+            <input type="submit">
+        </form>
+        <p>
+	</p>
+</html>
+"""
+
